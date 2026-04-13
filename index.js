4345240ff16f3826798d854bc700c66f34cdda7a
U5KUN <87700116+U5KUN@users.noreply.github.com>
2026-04-13 18:45:36 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.post('/UpdateLastLoginedInfo', (req, res) => {
+    const entityProfile = req.body.CallerEntityProfile;
+
+    console.log("PlayFabからのデータ:", req.body);
+
+    const resultPayload = {
+        "PlayerProfile": {
+          "AdCampaignAttributions": null,
+          "AvatarUrl": null,
+          "BannedUntil": null,
+          "ContactEmailAddresses": null,
+          "Created": null,
+          "DisplayName": null,
+          "ExperimentVariants": null,
+          "LastLogin": "2026-04-05T09:44:24.171Z",
+          "LinkedAccounts": null,
+          "Locations": null,
+          "Memberships": null,
+          "Origination": null,
+          "PlayerId": entityProfile.Lineage.MasterPlayerAccount,
+          "PublisherId": "3EEAAB5761592451",
+          "PushNotificationRegistrations": null,
+          "Statistics": null,
+          "Tags": null,
+          "TitleId": "2C099",
+          "TotalValueToDateInUSD": null,
+          "ValuesToDate": null
+        },
+        "TitlePlayerId": entityProfile.Entity.Id,
+        "UserTestGroup": 0,
+        "PresetList": null
+    };
+    
+    res.status(200).json(resultPayload);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
